d0ecb2df18a54b89f6e7761cbfd5fecf4a96534a
ianho36999 <ian.cmho@gmail.com>
2024-01-29 20:42:26 +0800

    Refactor to correct file name

--- app/src/main/java/com/example/mobileassignment/MainActivity.kt
+++ app/src/main/java/com/example/mobileassignment/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mobileassignment
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +11,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.loginBtn)
+        btn.setOnClickListener{
+            goLoginPage()
+        }
+    }
+
+    private fun goLoginPage() {
+        val intent = Intent(this@MainActivity, LoginPage::class.java)
+        startActivity(intent)
     }
 }
+
+
